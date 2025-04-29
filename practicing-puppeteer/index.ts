7b429495e134fb23d2b4778c453d059ac2639107
Prantik <prantiknoor@gmail.com>
2025-04-29 19:27:48 +0600

    Learned some important puppeteer methods

--- practicing-puppeteer/index.ts
+++ practicing-puppeteer/index.ts
@@ -1,4 +1,5 @@
 import { setTimeout } from "node:timers/promises"
+import { faker } from "@faker-js/faker"
 import puppeteer from "puppeteer"
 
 async function main() {
@@ -8,42 +9,74 @@ async function main() {
         headless: false
     })
     const page = (await browser.pages())[0]
-    await page.goto("https://monkeytype.com/", { waitUntil: "load" })
 
-    await page.click(".acceptAll")
-    await setTimeout(1000)
-
-    // await page.evaluate(() => {
-    //     document.querySelector("button[mode='words']").click()
-    //     document.querySelector("button[wordcount='custom']").click()
-    //     // document.querySelector("#customWordAmountModal input").value = 100
-    //     // document.querySelector("#customWordAmountModal button").click()
-    // })
-    // await setTimeout(200)
-    // await page.type("#customWordAmountModal input", "100")
-    // await page.keyboard.press("Enter")
-    // await setTimeout(500)
+    /*  await page.goto("https://monkeytype.com/", { waitUntil: "load" })
     
-    // const textToType = await page.evaluate(() => [...document.querySelectorAll('.word')].map(el => el.textContent).join(" "))
-    const textToType = (await page.$$eval(".word", els => els.map(el => el.textContent))).join(" ")
+        await page.click(".acceptAll")
+        await setTimeout(1000)
+    
+        await page.evaluate(() => {
+            document.querySelector("button[mode='words']").click()
+            document.querySelector("button[wordcount='custom']").click()
+            // document.querySelector("#customWordAmountModal input").value = 100
+            // document.querySelector("#customWordAmountModal button").click()
+        })
+        await setTimeout(200)
+        await page.type("#customWordAmountModal input", "100")
+        await page.keyboard.press("Enter")
+        await setTimeout(500)
+        
+        const textToType = await page.evaluate(() => [...document.querySelectorAll('.word')].map(el => el.textContent).join(" "))
+        const textToType = (await page.$$eval(".word", els => els.map(el => el.textContent))).join(" ")
+    
+        await page.type("#words", textToType, { delay: 50 })
+        await setTimeout(500)
+    
+        const speed = await page.$eval(".bottom", (el) => el.getAttribute("aria-label"));
+    
+        console.log("Speed:", speed);
+    
+    
+        await page.keyboard.down("a")
+        await page.keyboard.up("a")
+    
+        const linkEl = await page.waitForSelector("a", { timeout: 10_000 })
+        await linkEl?.click()
+    
+        await page.waitForNetworkIdle()
+    */
 
-    await page.type("#words", textToType, { delay: 50 })
-    await setTimeout(500)
+    // const getInnerText = (selector) => document.querySelector(selector)?.innerText;
+    // console.log(await page.evaluateOnNewDocument(() => document.querySelector('h1').innerText = "Hello World"));
 
-    const speed = await page.$eval(".bottom", (el) => el.getAttribute("aria-label"));
+    // await page.setRequestInterception(true)
 
-    console.log("Speed:", speed);
+    await page.goto("https://www.iana.org/help/example-domains")
 
+    const logoReq = await page.waitForResponse(req => req.url().includes("logo"))
+    await logoReq?.ok()
+    // await page.exposeFunction('md5', (text) =>
+    //     require('crypto').createHash("md5").update(text).digest('hex')
+    // )
+    // await page.exposeFunction('randomName', () => {
+    //     return faker.internet.username()
+    // })
 
-    // await page.keyboard.down("a")
-    // await page.keyboard.up("a")
+    // await page.evaluate(async () => {
+    //     // const linkText = document.querySelector('a')?.innerText
+    //     // const hash = await window.md5(linkText)
+    //     // document.querySelector('h1').innerText = hash
+    //     document.querySelector('h1').innerText = await window.randomName()
+    // })
 
-    // const linkEl = await page.waitForSelector("a", { timeout: 10_000 })
-    // await linkEl?.click()
+    // await page.addStyleTag({ content: "html { background: red }" })
 
-    // await page.waitForNetworkIdle()
+    // const linkText = await page.evaluate(getInnerText, 'h1');
+    // const hash = require('node:crypto').createHash("md5").update(linkText).digest('hex');
 
-    await browser.close()
+    // await page.evaluate((hash) => document.querySelector('h1').innerText = hash, hash)
+
+    // await browser.close()
     console.timeEnd("Done")
 }
 
